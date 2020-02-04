b9cfa47ef2b4e99344651f879697d06aa8f0c3c8
cimichaelm <michaelm@corp.citenet.net>
2020-02-04 09:56:39 -0500

    changes for python 3 compatibility

--- openvas_lib/utils.py
+++ openvas_lib/utils.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
 
 """
 Utils functions, like timer and random string generator.
@@ -23,7 +24,6 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """
 
-from __future__ import print_function
 
 from random import choice
 from threading import Event, Timer
