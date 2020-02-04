0ccf7b9e857fbe011064e976f3f5b16da764f26d
cimichaelm <michaelm@corp.citenet.net>
2020-02-04 09:56:04 -0500

    changes for python 3 compatibility

--- openvas_lib/utils.py
+++ openvas_lib/utils.py
@@ -23,13 +23,14 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """
 
+from __future__ import print_function
+
 from random import choice
 from threading import Event, Timer
 from string import ascii_letters, digits
 
 import time
 from datetime import date, datetime
-from __future__ import print_function
 
 #------------------------------------------------------------------------------
 #
